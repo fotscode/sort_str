99e3d9e63f12116ab6d143f642863a9737b2185f
fotscode <facundotomatis@gmail.com>
2021-07-05 13:03:51 -0300

    documented .h files

--- include/file_functions.h
+++ include/file_functions.h
@@ -7,12 +7,12 @@
 #define ERROR_CONFLICT 2 // exit code when there's a conflict of orders
 #define ERROR_INDEX 3 // exit code when non valid index is specified
 
-#define STRING_SIZE 512
+#define STRING_SIZE 512 // string size
 
 // displays help text
 void help(char *filename);
 
-// closes in and out if they're not stdin or stdout respectively
+// closes in and out if they're not stdin nor stdout respectively
 void my_close(FILE *in,FILE *out);
 
 // returns file's number of lines
--- include/str_vector_expanded.h
+++ include/str_vector_expanded.h
@@ -4,10 +4,13 @@
 #include <stdio.h>
 #include "str_vector.h"
 
+// creates vector with data from in
 void str_vector_create(str_vector_t *vector,FILE *in);
 
+// prints vector's content
 void str_vector_print(str_vector_t vector,FILE *out);
 
+// frees all the strings that were allocated by the process of creation
 void str_vector_free_all_str(str_vector_t *vector);
 
 #endif
