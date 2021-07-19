e970558e0c8ebc841f122c47e742eb4c82f6cf07
fotscode <facundotomatis@gmail.com>
2021-07-19 04:05:03 -0300

    minor fixes, now strings only occupy what they need

--- str_vector_expanded.c
+++ str_vector_expanded.c
@@ -5,24 +5,37 @@
 #include <string.h>
 
 void str_vector_create(str_vector_t *vector,FILE *in){
-  char aux[BUFSIZ];
+  if (!in) {
+    fprintf(stderr,"WARNING: file is equal to NULL, vector wasn't created\n");
+    return;
+  }
+  *vector=str_vector_new();
+  char aux[STRING_SIZE];
   while (fgets(aux,sizeof(char)*STRING_SIZE,in)){
-    char *s=malloc(sizeof(char)*STRING_SIZE);
+    char *s=malloc(sizeof(char)*(strlen(aux)+1));
     strcpy(s,aux);
     str_vector_append_sorted(vector,s,SEQ);
   }
 }
 
 void str_vector_print(str_vector_t vector,FILE *out){
+  if (!out){
+    fprintf(stderr,"WARNING: file is equal to NULL, vector wasn't printed\n");
+    return;
+  }
   int i;
   for (i=0;i<vector.size;i++) {
-    fputs(str_vector_get(&vector,i),out);
+    if(str_vector_get(&vector,i))
+      fputs(str_vector_get(&vector,i),out);
   }
 }
 
 void str_vector_free_all_str(str_vector_t *vector){
   int i;
   for(i=0;i<vector->size;i++){
-    free(str_vector_get(vector,i));
+    if (str_vector_get(vector,i)){
+      free(str_vector_get(vector,i));
+      str_vector_set(vector,i,NULL);
+    }
   }
 }
