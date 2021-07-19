04e7cd8ad8c38cdd84646f01fc1993bc9b08f56b
fotscode <facundotomatis@gmail.com>
2021-07-19 04:01:08 -0300

    minor fixes to all functions

--- file_functions.c
+++ file_functions.c
@@ -6,23 +6,26 @@ void help(char *filename){
   printf("Usage: %s [OPTIONS]...\n",filename);
   FILE *help=fopen("help.txt","r");
   if (!help){
-    my_close(help,stdout);
     fprintf(stderr,"%s: help.txt : Error opening file",filename);
     exit(ERROR_FILE);
   }
-  char s[BUFSIZ]="\0";
+  char s[128]="\0";
   while (fgets(s,sizeof(s),help)) printf("%s",s);
-  my_close(help,stdout);
+  fclose(help);
 }
 
 void my_close(FILE *in,FILE *out){
-  if (in!=stdin) fclose(in);
-  if (out!=stdout) fclose(out);
+  if (in) fclose(in);
+  if (out) fclose(out);
 }
 
 int count_lines(FILE *in){
+  if (!in) {
+    fprintf(stderr,"WARNING: file is equal to NULL, return value is 0\n");
+    return 0;
+  }
   int c=0;
-  char s[BUFSIZ]="\0";
+  char s[STRING_SIZE]="\0";
   while (fgets(s,sizeof(s),in)) c++;
   return c;
 }
